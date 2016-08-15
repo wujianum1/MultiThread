c6a02d6c6a953caa4125c4dde9f0b812ccb49782
jiajia <wujiajia@wujiajiademac.local>
2016-08-16 00:45:58 +0800

    Initial Commit

---
+++ MultiThread/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultiThread
+//
+//  Created by 吴嘉佳 on 16/8/16.
+//  Copyright © 2016年 吴嘉佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MultiThread/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MultiThread
+//
+//  Created by 吴嘉佳 on 16/8/16.
+//  Copyright © 2016年 吴嘉佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
